c6550337d2bea62398cd6460e99d17c51ad35cf4
Mohsen <mohsen63296329@gmail.com>
2024-12-10 23:31:20 +0330

    Config tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {},
+
+		container: {
+			center: true,
+			screens: {
+				sm: { min: '640px' },
+				md: { min: '768px' },
+				lg: { min: '1024px' },
+				xl: { min: '1280px' },
+				'2xl': { min: '1480px' },
+			},
+		},
+
+		colors: {
+			primary: '#F9F1E7',
+		},
 	},
 	plugins: [],
 }
