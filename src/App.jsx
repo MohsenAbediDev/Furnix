2f18e1224230ad768eff5422239d5d653f9ec41e
Mohsen <mohsen63296329@gmail.com>
2024-12-11 00:14:33 +0330

    Add react-router-dom and Routes file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-function App() {
+import { useRoutes } from 'react-router-dom'
+import Routes from './Routes'
 
-  return (
-    <>
-    <h1>salam</h1>
-    </>
-  )
+function App() {
+	const routes = useRoutes(Routes)
+	return (
+		<>
+			<h1>salam</h1>
+
+			{routes}
+		</>
+	)
 }
 
 export default App
