f5685c50310ec4165472be9a3847984844622a4c
shirpalang <shirpalang@gmail.com>
2024-12-11 14:46:06 +0330

    set default routes adress

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,8 @@
+import Home from "./page/Home"
 
-
-const routes = []
+const routes = [
+  {path: '/' , element: <Home/>},
+  {path: '/shop' , element: <h1>shop</h1>},
+]
 
 export default routes
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return <>
-  <p>home page</p>
+    <p>home page</p>
   </>
 }
