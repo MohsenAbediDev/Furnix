dad328d38698c1801b806603f7822424b00be86e
shirpalang <shirpalang@gmail.com>
2024-12-11 17:32:35 +0330

    add range component

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header"
+import Range from "../components/Range"
 
 export default function Home() {
   return <>
     <div className="w-full">
       <Header />
+      <Range />
     </div>
   </>
 }
