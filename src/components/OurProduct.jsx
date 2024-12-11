94a8e17bcafebfccb7399810f6c297d1c1f02a44
shirpalang <shirpalang@gmail.com>
2024-12-11 18:07:58 +0330

    add OurProduct component

---
+++ src/components/OurProduct.jsx
@@ -0,0 +1,9 @@
+export default function OurProduct() {
+  return <>
+    <section>
+      <div>
+        <p>Our Product</p>
+      </div>
+    </section>
+  </>
+}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import Range from "../components/Range"
+import OurProduct from "../components/OurProduct"
 
 export default function Home() {
   return <>
     <div className="w-full">
       <Header />
       <Range />
+      <OurProduct />
     </div>
   </>
 }
