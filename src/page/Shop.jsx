c21ed66b8292af1a3ad0bd1c7684f3cf5ed9568e
Mohsen <mohsen63296329@gmail.com>
2024-12-12 02:05:28 +0330

    Complete Shop page

--- src/page/Shop.jsx
+++ src/page/Shop.jsx
@@ -1,9 +1,61 @@
+import { useState } from 'react'
 import PageBanner from '../components/PageBanner'
+import Products from '../components/Products'
 
 function Shop() {
+	const [resultCount, setResultCount] = useState(16)
+	const [sort, setSort] = useState('Default')
+
 	return (
 		<div>
+			{/* Banner */}
 			<PageBanner />
+
+			{/* Filter and Display Bar */}
+			<div className='bg-primary h-16 px-32 flex-center-between '>
+				{/* Filter section */}
+				<div className='flex-center gap-x-3'>
+					{/* Filter */}
+					<img
+						src='public/icons/FilterSetting.png'
+						className='cursor-pointer'
+					/>
+					<span className='font-semibold cursor-pointer'>Filter</span>
+					<img src='public/icons/Grid.png' className='cursor-pointer' />
+					<img src='public/icons/ViewList.png' className='cursor-pointer' />
+
+					<span className='font-light mx-2'>|</span>
+
+					{/* Results */}
+
+					<div className='flex-center gap-x-1 text-sm cursor-pointer'>
+						Showing
+						<span>1-{resultCount}</span>
+						of
+						<span>32</span>
+						results
+					</div>
+				</div>
+
+				{/* Display section */}
+				<div className='flex-center gap-x-3'>
+					{/* Filter */}
+					<div className='flex-center gap-x-2 text-sm cursor-pointer'>
+						Show
+						<input
+							type='number'
+							className='bg-white w-7 h-7 outline-none text-center rounded-sm'
+							defaultValue={resultCount}
+						/>
+						Sort by
+						<select className='outline-none p-1 cursor-pointer rounded-sm'>
+							<option>Default</option>
+						</select>
+					</div>
+				</div>
+			</div>
+
+			<Products />
 		</div>
 	)
 }
