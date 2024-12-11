66be899ab25f16da40f8a721c6b9ae7061a356a2
shirpalang <shirpalang@gmail.com>
2024-12-11 18:11:57 +0330

    add productcard component

--- src/components/OurProduct.jsx
+++ src/components/OurProduct.jsx
@@ -1,8 +1,12 @@
+import ProductCard from "./ProductCard"
+
 export default function OurProduct() {
   return <>
     <section>
-      <div>
-        <p>Our Product</p>
+      <div className="flex justify-center">
+        <p className="my-12 text-3xl font-semibold">Our Product</p>
+        <div>
+        </div>
       </div>
     </section>
   </>
