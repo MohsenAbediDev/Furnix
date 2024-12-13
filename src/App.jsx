485a8c61fa6885dd09c691b69ba3e55c8979b2a1
Mohsen <mohsen63296329@gmail.com>
2024-12-13 14:25:48 +0330

    Create scroll to top btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useRoutes } from 'react-router-dom'
+import ScrollToTop from 'react-scroll-to-top'
 import Routes from './Routes'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -10,6 +11,13 @@ function App() {
 			<Navbar />
 
 			{routes}
+
+			<ScrollToTop
+				className='flex-center rounded-full bg-primary'
+				viewBox='0 0 24 24'
+				svgPath='M11.9999 10.8284L7.0502 15.7782L5.63599 14.364L11.9999 8L18.3639 14.364L16.9497 15.7782L11.9999 10.8284Z'
+				smooth
+			/>
 
 			<Footer />
 		</>
