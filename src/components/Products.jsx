8922ac309ffda45d842b974062d8a821e5948665
Mohsen <mohsen63296329@gmail.com>
2025-03-21 22:49:20 +0330

    Create loading animation in Products.jsx component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -14,8 +14,40 @@ function Products({ title, usePagination }) {
 		}
 	}, [dispatch, items.length])
 
-	if (loading) return <p className='font-bold text-xl text-center'>loading ...</p>
-	if (error) return <p className='font-bold text-xl text-center'>Error!</p>
+	if (error) return <p className='font-bold text-xl text-center mt-10'>Error! {error}</p>
+
+	if (loading) {
+		return (
+			<section className='flex-start-center'>
+				<div
+					className={`w-10/12 sm:w-11/12 flex-center-start flex-col ${
+						!title && 'mt-8'
+					}`}>
+					{/* Show title */}
+					{title && <p className='my-12 text-3xl font-semibold'>{title}</p>}
+
+					{/* Show products */}
+					<div className='grid grid-cols-4 sm:grid-cols-2 gap-5 sm:gap-x-5'>
+						{[...Array(8)].map((_, i) => (
+							<div
+								key={i}
+								className='w-[245px] sm:w-[190px] h-[383px] sm:h-[350px] bg-card relative overflow-hidden rounded-lg flex flex-col'>
+								{/* Product Image */}
+								<div className='w-full h-[250px] sm:h-[200px] bg-skeleton animate-pulse rounded-t-lg'></div>
+
+								{/* Product Information */}
+								<div className='w-full p-4 flex flex-col gap-y-3'>
+									<div className='w-3/4 h-6 bg-skeleton animate-pulse rounded-md'></div>
+									<div className='w-1/2 h-5 bg-skeleton animate-pulse rounded-md'></div>
+									<div className='w-1/4 h-6 bg-skeleton animate-pulse rounded-md'></div>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
+		)
+	}
 
 	return (
 		<section id='products' className='flex-start-center'>
