642df14845a7a61c5cabe91b8b399bc4231223f7
Mohsen <mohsen63296329@gmail.com>
2025-04-04 15:51:43 +0330

    Clean Image collage in Home page

--- src/components/FuniroFurniture.jsx
+++ src/components/FuniroFurniture.jsx
@@ -2,23 +2,15 @@ function FuniroFurniture() {
 	return (
 		<section>
 			<div>
-				<div className='flex flex-col items-center font-semibold my-9'>
+				<div className='flex flex-col items-center font-semibold mt-9 '>
 					<p className='text-base text-gray'>Share your setup with</p>
 					<p className='text-3xl'>#FurniroFurniture</p>
 				</div>
 
-				{/* Images */}
-				<div className='grid grid-cols-5 gap-6 justify-items-end items-end'>
-					<img src='/images/Rectangle 37.png' />
-					<img src='/images/Rectangle 38.png' />
-					<img src='/images/Rectangle 39.png' />
-					<img src='/images/Rectangle 40.png' />
-					<img src='/images/Rectangle 41.png' />
-					<img src='/images/Rectangle 44.png' />
-					<img src='/images/Rectangle 68.png' />
-					<img src='/images/Rectangle 69.png' />
-					<img src='/images/Rectangle 70.png' />
-				</div>
+				<img
+					src='/images/Share.png'
+					className='bg-cover mb-5 select-none drag-none'
+				/>
 			</div>
 		</section>
 	)
